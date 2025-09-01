bf55e7db72faf1bb29f0f9ddfcf7a8fb0328a1c1
Jhonatan Gomes Moura <166958516+jhonatangomes@users.noreply.github.com>
2025-09-01 16:20:18 -0300

    Novo exercícios

--- Modulo 01 - Fundamentos/ex031.py
+++ Modulo 01 - Fundamentos/ex031.py
@@ -2,6 +2,11 @@ cores = {'limpa':'\033[m',
         'azul':'\033[34m',
         'amarelo':'\033[33m',
         'pretobranco':'\033[7;30m'}
-nome = 'jhor ci jsh skn'
-n = nome.split()
-print(n[0], n[-1])
+
+dest = int(input('Qual é a distância de sua viagem? '))
+print('Você está prestes a começar uma viagem de {}km'.format(dest))
+if dest < 200:
+        valor = dest * 0.50
+else:
+        valor = dest * 0.45
+print('E o valor da sua passagem será de R${:.2f}'.format(valor))
