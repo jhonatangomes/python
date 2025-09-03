db9ea60a2f5e4699e72236b718e37ee158a7d721
Jhonatan Gomes Moura <166958516+jhonatangomes@users.noreply.github.com>
2025-09-03 20:07:31 -0300

    novo exercícios

---
+++ Modulo 02 - Estrutura de Controle/aula for.py
@@ -0,0 +1,29 @@
+'''for c in range(1 , 6):
+    print('Oi')
+print('FIM')'''
+
+'''
+for c in range(1 , 6):
+    print(c)
+print('FIM')'''
+'''
+for c in range(6, 0, -1):
+    print(c)
+print('FIM')'''
+'''
+for c in range(0, 7, 2):
+    print(c)
+print('FIM')'''
+'''
+i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f, p):
+    print(c)
+print('Fim')'''
+
+s = 0
+for c in range(0, 4):
+    n = int(input('Digite um valor: '))
+    s = s + n
+print('A soma dos valores digitados são {}'.format(s))
